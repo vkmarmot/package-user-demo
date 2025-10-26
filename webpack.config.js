516ec6219df1c2c37e25548bc46a2effd751d4c3
Vladimir Korneev <vkmarmot@yandex.ru>
2025-10-26 18:51:46 +0300

    serve mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
+import { dirname, resolve, join } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,12 @@ export default {
 
   mode: "development",
 
+  devServer: {
+    static: {
+      directory: join(__dirname, "public"),
+    },
+    port: 9000,
+  },
   // Выходной файл
   output: {
     filename: "index.js",
